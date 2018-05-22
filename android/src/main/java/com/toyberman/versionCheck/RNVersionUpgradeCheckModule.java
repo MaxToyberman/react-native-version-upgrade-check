f57efe023d7433cb243e379159055cafad883efc
Maxim Toyberman <>
2018-05-22 07:22:03 +0300

    Add ios check

--- android/src/main/java/com/toyberman/versionCheck/RNVersionUpgradeCheckModule.java
+++ android/src/main/java/com/toyberman/versionCheck/RNVersionUpgradeCheckModule.java
@@ -1,63 +1,65 @@
-
 package com.toyberman.versionCheck;
 
 import android.content.pm.PackageManager;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
 
 public class RNVersionUpgradeCheckModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNVersionUpgradeCheckModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @Override
-  public String getName() {
-    return "RNVersionUpgradeCheck";
-  }
-
-
-  public boolean isFirstInstall() {
-    try {
-      long firstInstallTime =   this.reactContext.getPackageManager().getPackageInfo(this.reactContext.getPackageName(), 0).firstInstallTime;
-      long lastUpdateTime = this.reactContext.getPackageManager().getPackageInfo(this.reactContext.getPackageName(), 0).lastUpdateTime;
-      return firstInstallTime == lastUpdateTime;
-    } catch (PackageManager.NameNotFoundException e) {
-      e.printStackTrace();
-      return true;
+    public RNVersionUpgradeCheckModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
     }
-  }
 
-
-
-  public boolean isInstallFromUpdate() {
-    try {
-      long firstInstallTime =   this.reactContext.getPackageManager().getPackageInfo(this.reactContext.getPackageName(), 0).firstInstallTime;
-      long lastUpdateTime = this.reactContext.getPackageManager().getPackageInfo(this.reactContext.getPackageName(), 0).lastUpdateTime;
-      return firstInstallTime != lastUpdateTime;
-    } catch (PackageManager.NameNotFoundException e) {
-      e.printStackTrace();
-      return false;
+    @Override
+    public String getName() {
+        return "RNVersionUpgradeCheck";
     }
-  }
-
-  @ReactMethod
-  public void isInstallFromUpdate(Callback callback) {
-
-    callback.invoke(isInstallFromUpdate());
-  }
 
 
-  @ReactMethod
-  public void isFirstInstall(Callback callback) {
-    callback.invoke(isFirstInstall());
-  }
+    public boolean isFirstInstall() {
+        try {
+            long firstInstallTime = this.reactContext.getPackageManager().getPackageInfo(this.reactContext.getPackageName(), 0).firstInstallTime;
+            long lastUpdateTime = this.reactContext.getPackageManager().getPackageInfo(this.reactContext.getPackageName(), 0).lastUpdateTime;
+            return firstInstallTime == lastUpdateTime;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return true;
+        }
+    }
 
+
+    public boolean isInstallFromUpdate() {
+        try {
+            long firstInstallTime = this.reactContext.getPackageManager().getPackageInfo(this.reactContext.getPackageName(), 0).firstInstallTime;
+            long lastUpdateTime = this.reactContext.getPackageManager().getPackageInfo(this.reactContext.getPackageName(), 0).lastUpdateTime;
+            return firstInstallTime != lastUpdateTime;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @ReactMethod
+    public void getVersionData(Callback callback) {
+
+        WritableMap map = Arguments.createMap();
+        if (isFirstInstall()) {
+            map.putBoolean("firstLaunch", true);
+        } else if (isInstallFromUpdate()) {
+            map.putBoolean("isUpdated", true);
+        } else {
+            map.putBoolean("sameVersion", true);
+        }
+
+        callback.invoke(map);
+    }
 
 }
--- ios/RNVersionUpgradeCheck.m
+++ ios/RNVersionUpgradeCheck.m
@@ -9,5 +9,40 @@
 }
 RCT_EXPORT_MODULE()
 
+
+RCT_EXPORT_METHOD(getVersionData:(RCTResponseSenderBlock)callback) {
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    NSString *currentAppVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    NSString *previousVersion = [defaults objectForKey:@"appVersion"];
+    
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+
+    
+    if (!previousVersion) {
+        
+        [defaults setObject:currentAppVersion forKey:@"appVersion"];
+        [defaults synchronize];
+        [dict setObject:[NSNumber numberWithBool:YES] forKey:@"firstLaunch"];
+
+    } else if ([previousVersion isEqualToString:currentAppVersion]) {
+        // same version
+        [dict setObject:[NSNumber numberWithBool:YES] forKey:@"sameVersion"];
+
+    } else {
+        // other version
+        [defaults setObject:currentAppVersion forKey:@"appVersion"];
+        [defaults synchronize];
+        [dict setObject:[NSNumber numberWithBool:YES] forKey:@"isUpdated"];
+
+    }
+    callback(@[[NSDictionary dictionaryWithObjects:dict.allValues
+                                           forKeys:dict.allKeys]]);
+
+    
+}
+
+
 @end
   
